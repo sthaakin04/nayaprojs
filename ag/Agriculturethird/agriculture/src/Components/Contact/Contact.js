40932bf36f1ad6c7db1de076c9c11f4811ee9a37
sthaakin04 <akinstha97@gmail.com>
2022-12-30 11:39:44 +0545

    contact us page

--- ag/Agriculturethird/agriculture/src/Components/Contact/Contact.js
+++ ag/Agriculturethird/agriculture/src/Components/Contact/Contact.js
@@ -3,57 +3,44 @@ import "./Contact.css"
 
 export default function Contact() {
   return (
-    <div id="contact">
-      <div className="background">
-        <div className="contactcontainer">
-          <div className="screen">
-            <div className="screen-header">
-              <div className="screen-header-left">
-                <div className="screen-header-button close"></div>
-                <div className="screen-header-button maximize"></div>
-                <div className="screen-header-button minimize"></div>
-              </div>
-              <div className="screen-header-right">
-                <div className="screen-header-ellipsis"></div>
-                <div className="screen-header-ellipsis"></div>
-                <div className="screen-header-ellipsis"></div>
-              </div>
-            </div>
-            <div className="screen-body">
-              <div className="screen-body-item left">
-                <div className="app-title">
-                  <span>CONTACT</span>
-                  <span>US</span>
-                </div>
-                <div className="app-contact">CONTACT INFO : +977 9812345678</div>
-              </div>
-              <div className="screen-body-item">
-                <div className="app-form">
-                  <div className="app-form-group">
-                    <input
-                      className="app-form-control"
-                      placeholder="NAME"
-                    />
-                  </div>
-                  <div className="app-form-group">
-                    <input className="app-form-control" placeholder="EMAIL" />
-                  </div>
-                  <div className="app-form-group">
-                    <input className="app-form-control" placeholder="CONTACT NO" />
-                  </div>
-                  <div className="app-form-group message">
-                    <input className="app-form-control" placeholder="MESSAGE" />
-                  </div>
-                  <div className="app-form-group buttons">
-                    {/* <button className="app-form-button">CANCEL</button> */}
-                    <button className="app-form-button">SEND</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <>
+    <div class="modal">
+    <div class="modal__container">
+      <div class="modal__featured">
+        <button type="button" class="button--transparent button--close">
+          {/* <svg class="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px" viewBox="0 0 32 32"> */}
+            <g><path fill="#ffffff" d="M1.293,15.293L11,5.586L12.414,7l-8,8H31v2H4.414l8,8L11,26.414l-9.707-9.707 C0.902,16.316,0.902,15.684,1.293,15.293z"></path> </g>
+            {/* </svg> */}
+          <span class="visuallyhidden">Return to Home Page</span>
+        </button>
+        <div class="modal__circle"></div>
+        <img src="https://images.unsplash.com/photo-1505471768190-275e2ad7b3f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" class="modal__product" />
+      </div>
+      <div class="modal__content">
+        <h2>Contact Us</h2>
+
+        <form>
+          <ul class="form-list">
+            <li class="form-list__row">
+              <label>Name</label>
+              <input type="text" name="" required="" />
+            </li>
+            <li class="form-list__row">
+              <label>Email</label>
+              <input type="text" name="" required="" />
+            </li>
+            <li class="form-list__row">
+              <label>Message</label>
+              <input type="text" name="" required="" />
+            </li>
+            <li>
+              <button type="submit" class="button">Submit</button>
+            </li>
+          </ul>
+        </form>
       </div>
     </div>
+  </div>
+    </>
   );
 }
