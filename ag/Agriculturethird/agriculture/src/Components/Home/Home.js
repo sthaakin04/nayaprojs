d41b3ec479c60b9a9171d252aeb8cea29a428918
sthaakin04 <akinstha97@gmail.com>
2022-12-30 10:28:28 +0545

    home contents

--- ag/Agriculturethird/agriculture/src/Components/Home/Home.js
+++ ag/Agriculturethird/agriculture/src/Components/Home/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { HashLink } from 'react-router-hash-link'
+import Contents from '../Contents/Contents'
 import './Home.css'
 export default function Home() {
   return (
+    <>
     <div id="home">
       <section className="box">
         <video src="static/images/fieldvideoall.mp4" autoPlay muted loop></video>
         <div className="content">
             <div className="info">
                 <h2>Like Agriculture<br/><span>Be Farmer!</span></h2>
-                <button type="button" class="btn btn-success">More Info</button>
+                <button type="button" className="btn btn-success">More Info</button>
             </div>
         </div>
       </section>
@@ -30,5 +32,8 @@ export default function Home() {
       </div>
   </div>
     </div>
+  <Contents/>
+    
+    </>
   )
 }
