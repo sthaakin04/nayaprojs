6677bbbada21e8ec471826c6fbebe85d9f5a1fb4
sthaakin04 <akinstha97@gmail.com>
2022-12-30 12:05:23 +0545

    removed unnecessary

--- ag/Agriculturethird/agriculture/src/Components/Home/Home.js
+++ ag/Agriculturethird/agriculture/src/Components/Home/Home.js
@@ -15,22 +15,6 @@ export default function Home() {
             </div>
         </div>
       </section>
-      <div className="about-us">
-      <div className="text">
-          <h2>Discover</h2>
-          <h3>Our Story</h3>
-          <div><i className="fas fa-asterisk"></i></div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolores sit ipsam incidunt soluta aspernatur itaque pariatur! Fuga id provident quas vitae, dolore error mollitia excepturi iusto repellat, blanditiis deleniti.</p>
-      </div>
-      <div className="image-container">
-          <div className="image image1">
-              <img src="static/images/ag1.png" alt="aboutus photo"/>
-          </div>
-          <div className="image image2">
-              <img src="static/images/ag2.png" alt="aboutus photo"/>
-          </div>
-      </div>
-  </div>
     </div>
   <Contents/>
     
