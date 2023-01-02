fe8c17642a3e00040caff79e75ff9b033addbb41
sthaakin04 <akinstha97@gmail.com>
2023-01-02 15:07:52 +0545

    new

--- ag/Agriculturefourth/agriculture/src/Components/Contents/Contents.js
+++ ag/Agriculturefourth/agriculture/src/Components/Contents/Contents.js
@@ -8,9 +8,9 @@ export default function Contents() {
   
   <div className="news-card">
     <a href="#" className="news-card__card-link"></a>
-    <img src="https://images.pexels.com/photos/127513/pexels-photo-127513.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className="news-card__image"/>
+    <img src="https://images.unsplash.com/photo-1632064535948-d387f1c8789c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=955&q=80" alt="" className="news-card__image"/>
     <div className="news-card__text-wrapper">
-      <h2 className="news-card__title">Amazing First Title</h2>
+      <h2 className="news-card__title">Boosting the production</h2>
       <div className="news-card__post-date">Jan 29, 2018</div>
       <div className="news-card__details-wrapper">
         <p className="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio, pariatur&hellip;</p>
@@ -21,9 +21,9 @@ export default function Contents() {
 
   <div className="news-card">
     <a href="#" className="news-card__card-link"></a>
-    <img src="https://images.pexels.com/photos/631954/pexels-photo-631954.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className="news-card__image"/>
+    <img src="https://images.unsplash.com/photo-1651592278720-fd9479be2a9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" className="news-card__image"/>
     <div className="news-card__text-wrapper">
-      <h2 className="news-card__title">Amazing Second Title that is Quite Long</h2>
+      <h2 className="news-card__title">Farm like u always wanted</h2>
       <div className="news-card__post-date">Jan 29, 2018</div>
       <div className="news-card__details-wrapper">
         <p className="news-card__excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam obcaecati ex natus nulla rem sequi laborum quod fugit&hellip;</p>
@@ -36,7 +36,7 @@ export default function Contents() {
     <a href="#" className="news-card__card-link"></a>
     <img src="https://images.pexels.com/photos/247599/pexels-photo-247599.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" className="news-card__image"/>
     <div className="news-card__text-wrapper">
-      <h2 className="news-card__title">Amazing Title</h2>
+      <h2 className="news-card__title">Farms</h2>
       <div className="news-card__post-date">Jan 29, 2018</div>
       <div className="news-card__details-wrapper">
         <p className="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis beatae&hellip;</p>
@@ -47,9 +47,9 @@ export default function Contents() {
 
   <div className="news-card">
     <a href="#" className="news-card__card-link"></a>
-    <img src="https://images.pexels.com/photos/248486/pexels-photo-248486.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className="news-card__image"/>
+    <img src="https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" className="news-card__image"/>
     <div className="news-card__text-wrapper">
-      <h2 className="news-card__title">Amazing Forth Title that is Quite Long</h2>
+      <h2 className="news-card__title">Work smarter</h2>
       <div className="news-card__post-date">Jan 29, 2018</div>
       <div className="news-card__details-wrapper">
         <p className="news-card__excerpt">Lorem ipsum dolor sit amet!</p>
@@ -60,9 +60,9 @@ export default function Contents() {
 
   <div className="news-card">
     <a href="#" className="news-card__card-link"></a>
-    <img src="https://images.pexels.com/photos/206660/pexels-photo-206660.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className="news-card__image"/>
+    <img src="https://images.unsplash.com/photo-1605000797499-95a51c5269ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="" className="news-card__image"/>
     <div className="news-card__text-wrapper">
-      <h2 className="news-card__title">Amazing Fifth Title</h2>
+      <h2 className="news-card__title">Work easier</h2>
       <div className="news-card__post-date">Jan 29, 2018</div>
       <div className="news-card__details-wrapper">
         <p className="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio&hellip;</p>
@@ -73,9 +73,9 @@ export default function Contents() {
 
   <div className="news-card">
     <a href="#" className="news-card__card-link"></a>
-    <img src="https://images.pexels.com/photos/210243/pexels-photo-210243.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className="news-card__image"/>
+    <img src="https://images.unsplash.com/photo-1654436192756-e6a4141cc23a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" className="news-card__image"/>
     <div className="news-card__text-wrapper">
-      <h2 className="news-card__title">Amazing 6<sup>th</sup> Title</h2>
+      <h2 className="news-card__title">Big farms</h2>
       <div className="news-card__post-date">Jan 29, 2018</div>
       <div className="news-card__details-wrapper">
         <p className="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia.</p>
